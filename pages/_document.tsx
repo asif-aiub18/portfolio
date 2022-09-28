5a2e918554390bc82213ea3960b78b24dcb7e9e4
asif-aiub18 <18-37992-2@student.aiub.edu>
2022-09-28 08:00:56 +0600

    Meta Tag Updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,32 +4,34 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta property="og:title" content="Ashik Rahman - Developer" />
+        <meta property="og:title" content="Asif Anower - Developer" />
         <meta
           property="og:image"
-          content="https://v2.ashrhmn.com/images/user1.jpeg"
+          content="https://asifanower.com/images/user1.jpeg"
         />
         <meta
           name="title"
-          content="Ashik Rahman | Fullstack and Blockchain Developer"
+          content="Asif Anower | Fullstack Developer"
         />
-        <meta name="keywords" content="Ashik Rahman" />
+        <meta name="keywords" content="Asif Anower" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="robots" content="index" />
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@ashrhmn7" />
+        <meta name="twitter:site" content="@asif_anower24" />
         <meta
           name="twitter:title"
-          content="Ashik Rahman | Fullstack and Blockchain Developer"
+          content="Asif Anower | Fullstack Developer"
         />
-        <meta property="fb:app_id" content="ashrhmn7" />
-        <meta property="og:image:alt" content="Ashik Rahman | Developer" />
+        <meta property="fb:app_id" content="anowert2020" />
+        <meta property="og:image:alt" content="Asif Anower | Developer" />
         <meta
           name="twitter:description"
-          content="23 years old and from Bangladesh. Development has been an interest for many years, and works with anything from Desktop App to Mobile App and fullstack development. All the skills and tools are self-taught over years of practice and expanding my horizons"
+          content="I am Asif Anower, 23 years old and from Bangladesh. For a considerable time, I have been interested in web development.
+          I enjoy discovering new stuff.I aim to learn more because I currently have a very restricted knowledge base.
+          My own passion in learning web development and participating in it is web app development."
         />
         <meta property="og:site_name" content="Ashik Rahman" />
-        <meta property="og:url" content="https://ashrhmn.com/" />
+        <meta property="og:url" content="https://asifanower.com/" />
         <meta
           property="og:description"
           content="23 years old and from Bangladesh. Development has been an interest for many years, and works with anything from Desktop App to Mobile App and fullstack development. All the skills and tools are self-taught over years of practice and expanding my horizons"
